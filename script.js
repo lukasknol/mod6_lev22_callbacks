62f86876cb1db0723beba31a599b14862f4b02e4
Lukas <79308537+lukasknol@users.noreply.github.com>
2021-04-21 12:58:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,39 @@
-/*
-Exercise 1:
-Write a function testNum that takes a number as an argument and returns a Promise that tests if the value is less than or greater than the value 10. Log the result to the console.
-*/
+// 1
+const huiswerkMaken = function (vak, callback) {
+    console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
+    callback();
+}
 
-const testNum = num => {
-    return new Promise((resolve, reject) => {
-        if (num > 10) {
-            resolve(num + ' is greater than 10');
-        } else {
-            reject(num + ' is less than 10');
-        }
-    });
-};
+// 2
+const huiswerkMaken = function (vak, callback) {
+    console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
+    callback();
+}
 
-testNum(9)
-    .then(result => console.log(result))
-    .catch(error => console.log(error));
+const klaarMetHuiswerk = function () {
+    console.log('Kijk Paps/Mams, ik ben klaar met mijn huiswerk!');
+}
 
-testNum(11)
-    .then(result => console.log(result))
-    .catch(error => console.log(error));
+// 3
+const huiswerkMaken = function (vak, callback) {
+    console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
+    callback();
+}
 
+const klaarMetHuiswerk = function () {
+    console.log('Kijk Paps/Mams, ik ben klaar met mijn huiswerk!');
+}
+huiswerkMaken('wiskunde', klaarMetHuiswerk);
 
-/*
-Exercise 2:
-Write two functions that use Promises that you can chain! The first function, makeAllCaps(), will take in an array of words and capitalize them, and then the second function, sortWords(), will sort the words in alphabetical order. If the array contains anything but strings, it should throw an error.
-Then call these functions by *chaining* the promises
-*/
+// 4
+const huiswerkMaken = function (vak, callback) {
+    console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
+    setTimeout(function () {
+        callback();
+    }, 3000);
+}
 
-const makeAllCaps = words => {
-    return new Promise((resolve, reject) => {
-        if (
-            words.every(word => {
-                return typeof word === 'string';
-            })
-        ) {
-            resolve(
-                sortWords(
-                    words.map(word => {
-                        return word.toUpperCase();
-                    })
-                )
-            );
-        } else {
-            reject('Not a string!');
-        }
-    });
-};
-
-const sortWords = words => {
-    return new Promise((resolve, reject) => {
-        if (words) {
-            resolve(words.sort());
-        } else {
-            reject('strings only!');
-        }
-    });
-};
-
-const theseAreWords = ['promise', 'practice', 'break'];
-
-makeAllCaps(theseAreWords)
-    .then(sortWords(theseAreWords))
-    .then(result => console.log(result))
-    .catch(error => console.log(error));
-
-const theseAreNotWords = [1, 'hello', 9];
-
-makeAllCaps(theseAreNotWords)
-    .then(sortWords(theseAreNotWords))
-    .then(result => console.log(result))
-    .catch(error => console.log(error));
-
-// call both functions, chain them together and log the result to the console
+const klaarMetHuiswerk = function () {
+    console.log('Kijk Paps/Mams, ik ben klaar met mijn huiswerk!');
+}
+huiswerkMaken('wiskunde', klaarMetHuiswerk);
